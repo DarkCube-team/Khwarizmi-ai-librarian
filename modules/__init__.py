8675af05aac11e54f7759935ff6290075b9e0391
Amir Gharegozlou <allmersupanii@gmail.com>
2025-11-19 19:42:31 +0330

    Create __init__.py

---
+++ modules/__init__.py
@@ -0,0 +1,13 @@
+from modules.base_handler import BaseHandler
+from modules.book_handler import BookHandler
+from modules.thesis_handler import ThesisHandler
+from modules.filter_manager import FilterManager
+from modules.regulations_handler import RegulationsHandler
+
+__all__ = [
+    'BaseHandler',
+    'BookHandler',
+    'ThesisHandler',
+    'FilterManager',
+    'RegulationsHandler'
+]
